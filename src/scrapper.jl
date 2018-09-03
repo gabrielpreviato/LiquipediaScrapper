88e4d321bf6271b5c3ca2ac259f55dfd1de0eff7
Gabriel Previato <gabriel.previato@gmail.com>
2018-09-03 14:47:42 -0300

    Maybe use python instead of julia

--- src/scrapper.jl
+++ src/scrapper.jl
@@ -6,11 +6,8 @@ ha = replace(split(url, "https://liquipedia.net/starcraft2/")[2], "/" => "_")
 raw_html = HTTP.request("GET", url)
 
 parsed_html = parsehtml(String(raw_html))
-t = tag(parsed_html.root)
 
-#open(pwd()+""
-
-local_path = joinpath(splitdir(@__DIR__)[1],"texts")
+local_path = joinpath(splitdir(@__DIR__)[1],"texts/")
 if !isdir(local_path)
     mkdir(local_path)
 end
@@ -19,3 +16,34 @@ url_to_txt = replace(split(url, "https://liquipedia.net/starcraft2/")[2], "/" =>
 open(local_path * url_to_txt, "w") do file
     println(file, parsed_html.root)
 end
+
+a = parsed_html.root
+b = a.children[2]
+c = b.children
+d = c[23]
+e = tag(d)
+f = attrs(d)
+function find_tag(tag::String, childs::Array{HTMLNode,1})
+    1
+end
+
+find_tag("ha", c)
+
+for i in 1:length(c)
+    if isa(c[i], HTMLText)
+        continue
+    end
+    for child in c[i].children
+        if isa(child, HTMLText)
+            continue
+        end
+        if haskey(attrs(child), "class") && get(attrs(child), "class", "") == "container-fluid main-content"
+            println(i)
+        end
+    end
+    # if isa(c[i], HTMLElement{:div})
+    #     println(attrs(c[i]))
+    # end
+end
+
+g = children(c[63])
--- texts/py.py
+++ texts/py.py
@@ -1,8 +1,22 @@
 from bs4 import BeautifulSoup
+import bs4
 import requests
 
 html_doc = requests.get('https://liquipedia.net/starcraft2/Copa_America_2018/Season_3')
 soup = BeautifulSoup(html_doc.text, 'html.parser')
 
-with open('/home/previato/Dropbox/julia_projects/LiquipediaScrapper.jl/textsCopa_America_2018_Season_3.html', 'w') as file:
+with open('/home/previato/Dropbox/julia_projects/LiquipediaScrapper.jl/texts/Copa_America_2018_Season_3.html', 'w') as file:
     file.write(soup.prettify())
+
+def has_group_match(tag):
+    if tag.name == 'tr':
+        for child in tag.children:
+            if isinstance(child, bs4.element.Tag) and has_class(child, 'matchlistslot'):
+                return True
+    return False
+
+def has_class(tag, class_name):
+    return tag.has_attr('class') and tag['class'][0] == class_name
+
+for i in soup.find_all(has_group_match):
+    print(i.prettify())
